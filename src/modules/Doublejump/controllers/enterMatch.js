f420a50d0dcb9c5ba3a2a774fc0c244ad04f3bb2
QsageHint <qsagehint0019@gmail.com>
2023-08-19 00:46:12 +0900

    fix: enterMatch with wallet again

--- src/modules/Doublejump/controllers/enterMatch.js
+++ src/modules/Doublejump/controllers/enterMatch.js
@@ -1,10 +1,31 @@
 import UserModel from "../../User/model";
 import DoublejumpModel from "../model";
 
+
 export const enterMatchController = async (req, res) => {
   const matchId=req.body['match-id'];
   const playerSize=req.body['player-size'];
-  const players=req.body['players'];
+  const originalPlayers=req.body['players'];
+
+  let players = [];
+
+  for (const result of originalPlayers) {
+    const { email } = result;
+
+    // Search UserModel by email to get the wallet-address
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      const { walletAddress } = user;
+
+      // Create a new dictionary with wallet-address
+      const player = {
+        "wallet-address": walletAddress,
+      };
+
+      players.push(player);
+    }
+  }
 
   try {
     const existingMatch=await DoublejumpModel.findOne({matchId});
@@ -12,16 +33,16 @@ export const enterMatchController = async (req, res) => {
         res.status(400).json({ message: 'Match with same Id already existed!' });
     }
     for( const player of players){
-        let user=await UserModel.findOne({ email: player["email"] });
+        let user=await UserModel.findOne({ wallet: player["wallet-address"] });
         if(user.paidMatchState===0) res.status(400).json({ message: "Can't enter the match!" });
     }
     for( const player of players){
-        let user=await UserModel.findOne({ email: player["email"] });
+        let user=await UserModel.findOne({ wallet: player["wallet-address"] });
         user.matchId=matchId;
         await user.save();
     }
     for( const player of players){
-        await UserModel.findOneAndUpdate({ email: player["email"] }, {paidMatchState: 0}, {
+        await UserModel.findOneAndUpdate({ wallet: player["wallet-address"] }, {paidMatchState: 0}, {
             new: true
         });
     }
