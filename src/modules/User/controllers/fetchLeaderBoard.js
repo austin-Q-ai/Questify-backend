03e399966ac93119494e553aa97e28d686e87dfc
QsageHint <qsagehint0019@gmail.com>
2023-08-31 03:35:07 +0900

    fix: adjust score limit to 15

--- src/modules/User/controllers/fetchLeaderBoard.js
+++ src/modules/User/controllers/fetchLeaderBoard.js
@@ -7,19 +7,19 @@ export const fetchLeaderBoardController = async (req, res) => {
   try {
     const showInfo = await UserModel.find(
       {},
-      { wallet: 1, totalScore: 1, totalXP: 1, totalStar:1, rewardKey:1 }
+      { wallet: 1, totalScore: 1, totalXP: 1, totalStar: 1, rewardKey: 1 }
     )
       .sort({ totalScore: -1 })
-      .limit(5);
+      .limit(15);
     const showXPInfo = await UserModel.find(
       {},
-      { wallet: 1, totalScore: 1, totalXP: 1, totalStar:1 , rewardKey:1}
+      { wallet: 1, totalScore: 1, totalXP: 1, totalStar: 1, rewardKey: 1 }
     )
       .sort({ totalXP: -1 })
       .limit(5);
-    const totalKeyInfo=await TotalkeyModel.find(
+    const totalKeyInfo = await TotalkeyModel.find(
       {},
-      {totalKey:1, claimedKey:1, totalRewards:1, claimedRewards:1}
+      { totalKey: 1, claimedKey: 1, totalRewards: 1, claimedRewards: 1 }
     ).limit(1);
     const tetrisInfo = await TetrisModel.find({})
       .sort({ updatedAt: -1 })
